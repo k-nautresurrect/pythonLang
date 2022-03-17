2f887a1483c4d27ef3f879e3119fb937fafe430a
k-nautresurrect <kushignite@gmail.com>
2022-03-17 11:13:46 +0530

    Added literals

---
+++ Basics/literals.py
@@ -0,0 +1,31 @@
+print(__name__)
+print(type(__name__))
+# print(namespace)
+
+'''
+Literals
+	numbers
+		- integers
+		- floats
+		- complex
+	String
+		- Single Quotes
+		- double Quotes
+		- triple Quotes
+		- Escape sequences
+		- Explicit line joining
+'''
+
+a = 20
+b = 123.4568987
+c = 23 + 89j
+
+print(type(a), type(b), type(c), sep = '\n')
+print(a + b + c)
+
+print(a + b)
+print(a * c)
+
+stra = "e"
+print(type(stra))
+
